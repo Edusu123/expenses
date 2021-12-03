936e70ce6059dec1c0ec49d750220acc96d5246b
eduardopires <eduardo.c.pires@outlook.com>
2021-12-03 08:37:29 -0300

    Usando TextField 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
         id: 't1',
@@ -82,16 +85,21 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(labelText: 'Título'),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(labelText: 'Valor (R\$)'),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                         textColor: Colors.purple,
                         child: Text('Nova transação'),
                       ),
