de2848b608bb0767a890802caa2d3147563868c0
eduardopires <eduardo.c.pires@outlook.com>
2021-12-03 09:47:38 -0300

    Lista com Rolagem

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,40 +10,46 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map((e) => Card(
-                child: Row(children: <Widget>[
-                  Container(
-                    child: Text(
-                      'R\$ ${e.value.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    padding: EdgeInsets.all(10),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        e.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions
+              .map((e) => Card(
+                    child: Row(children: <Widget>[
+                      Container(
+                        child: Text(
+                          'R\$ ${e.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        padding: EdgeInsets.all(10),
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(e.date),
-                        style: TextStyle(color: Colors.grey),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            e.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat('d MMM y').format(e.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
                       )
-                    ],
-                  )
-                ]),
-              ))
-          .toList(),
+                    ]),
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
