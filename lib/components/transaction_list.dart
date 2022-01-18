466ff2931f49701e3191ca336a1aa2685da85a89
eduardopires <eduardo.c.pires@outlook.com>
2022-01-18 10:27:02 -0300

    Extraindo o Widget

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,7 +1,7 @@
+import 'transaction_item.dart';
+
 import 'package:expenses/models/transaction.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -33,35 +33,7 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(child: Text('R\$${tr.value}')),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                  trailing: MediaQuery.of(context).size.width > 480
-                      ? FlatButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(Icons.delete),
-                          label: const Text('Excluir'),
-                          textColor: Theme.of(context).errorColor)
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => onRemove(tr.id),
-                          color: Theme.of(context).errorColor,
-                        ),
-                ),
-              );
+              return TransactionItem(tr: tr, onRemove: onRemove);
             });
   }
 }
