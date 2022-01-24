e84931f96078d32d804d74724fbd02d70e21d9ab
eduardopires <eduardo.c.pires@outlook.com>
2022-01-24 08:46:02 -0300

    Sorteando cores para o CircleAvatar de cada transação

--- lib/components/transaction_item.dart
+++ lib/components/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key? key,
     required this.tr,
@@ -13,32 +15,58 @@ class TransactionItem extends StatelessWidget {
   final void Function(String p1) onRemove;
 
   @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  static const colors = [
+    Colors.red,
+    Colors.purple,
+    Colors.orange,
+    Colors.blue,
+    Colors.black,
+    Colors.indigo
+  ];
+
+  Color _backgroundColor = Colors.white;
+
+  @override
+  void initState() {
+    super.initState();
+
+    int i = Random().nextInt(6);
+    print(i);
+    _backgroundColor = colors[i];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _backgroundColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6.0),
-            child: FittedBox(child: Text('R\$${tr.value}')),
+            child: FittedBox(child: Text('R\$${widget.tr.value}')),
           ),
         ),
         title: Text(
-          tr.title,
+          widget.tr.title,
           style: Theme.of(context).textTheme.headline6,
         ),
-        subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+        subtitle: Text(DateFormat('d MMM y').format(widget.tr.date)),
         trailing: MediaQuery.of(context).size.width > 480
             ? FlatButton.icon(
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
                 icon: const Icon(Icons.delete),
                 label: const Text('Excluir'),
                 textColor: Theme.of(context).errorColor)
             : IconButton(
                 icon: Icon(Icons.delete),
-                onPressed: () => onRemove(tr.id),
+                onPressed: () => widget.onRemove(widget.tr.id),
                 color: Theme.of(context).errorColor,
               ),
       ),
